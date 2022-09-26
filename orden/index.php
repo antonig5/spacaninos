301aaea864dba087a4a17cc926c1f4eb05fe3fb7
Haroldmakaveli <93088235+Haroldmakaveli@users.noreply.github.com>
2022-09-26 07:40:56 -0500

    Update index.php
    cambios de etiquetas de texto

--- orden/index.php
+++ orden/index.php
@@ -13,6 +13,7 @@ $datos = $orden->fetchAll(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="../css/orden.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
