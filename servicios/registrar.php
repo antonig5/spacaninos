3a7141d733be4b7695679d7d0d885e4a634481a4
Nicolas 123 <100376064+Nicolastafur@users.noreply.github.com>
2022-09-27 21:21:55 -0500

    Add files via upload

--- servicios/registrar.php
+++ servicios/registrar.php
@@ -1,24 +1,45 @@
 <?php
-include("../conexion/conexion.php");
-if(isset($_GET['Registrar'])){
-       
-        
-        $nomM = $_GET['nomS'];
-        $precio = $_GET['Pre'];
-        
-    
-        ?>
-        
-        <?php 
-        
-        
-        $sql="INSERT INTO servicios (  nombre_servicio,precio_servicio) values (:nomS,:pre)";
-        $resultado=$base_de_datos->prepare($sql);//$base es el nombre de la conexión
-        $resultado->execute(array(  ":nomS"=>$nomM, ":pre"=>$precio));
+    require("../conexion/conexion.php");
 
-        echo"<script>alert('Se realizo su exito correcto.')</script>";
-        echo"<script>window.location='tablaS.php'</script>";
-    
-     
-    }
+    if(isset($_POST['bt'])){
+
+		$apellido=                      $_POST['apellido'];
+        $nombre=                        $_POST['nomb'];
+
+		?>
+	
+		<?php 
+		$sql="INSERT INTO servicios ( nombre_servicio,precio_servicio) values ( :no, :em)";
+		$resultado=$base_de_datos->prepare($sql);//$base es el nombre de la conexión
+		$resultado->execute(array(":no"=>$nombre,":em"=>$apellido));
+        echo"<script>alert('Se registro correctamente')</script>";
+        echo"<script>window.location='./tablaS.php'</script>";
+	}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/res_clientes.css">
+    <title>Registrar</title>
+</head>
+<body>
+    <div id="res">
+        <div id="h2"><h2 id="h2_1">Registro </h2></div>
+        <form method="post">
+
+        <div id="lab2"><label>Nombre servicio:</label></div>
+        <input id="inp2" name="nomb" type="text" placeholder="Ingrese nombre">
+
+        <div id="lab3"><label>Precio servicio:</label></div>
+        <input id="inp3" name="apellido" type="number" placeholder="Ingrese precio">
+
+        <input id="bot" type="submit" name="bt" value="Registrar">
+    </form>
+    </div>
+</body>
+</html>
--- servicios/tablaS.php
+++ servicios/tablaS.php
@@ -1,111 +1,145 @@
+<?php
+   session_start();
+   include_once("../conexion/conexion.php");
+
+   $cedu1=                 $_SESSION['cedu'];
+   $nombre1=               $_SESSION['nomb'];
+   $tipo=                  $_SESSION['tipo'];
+   $tip=                   $_SESSION['tip'];
+
+    $sentencia1="SELECT * FROM servicios";
+    $resultado=$base_de_datos->prepare($sentencia1);
+    $resultado->execute();
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="#">
-    <title>Panel  de  registros de mascotas</title>
-    <link href="../css/tablas.css" rel="stylesheet" type="text/css">
-
-</head>
-<header>
-    
-   
-</header>
-<body>
-    <?php
-    
-    include("../conexion/conexion.php");
-
-    
-    //--------------paginación-------------
-    $registros=3;//indica que se van a ver 3 registro por página
-    if(isset($_GET["pagina"])){
-        if($_GET["pagina"]==1){
-            header("Location:tablaM.php");
-        }else{
-            $pagina=$_GET["pagina"];
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- CSS only -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href=" https://unpkg.com/tailwindcss@1.2.0/dist/tailwind.min.css">
+    <title>cliente</title>
+    <style>
+        a{
+            color:white;
         }
-    }else{
-        $pagina=1;//muestra página en la que estamos cuando se carga por primera vez
-    }
+        a:hover{
+            color:aqua;
+        }
+        #a{
+            color:black;
+        }
+        table{
+            margin-top:20px;
+            width:230px;
+        }
+        h1{
+          font-size:25px;
+          margin-top:40px;
+        }
+        h2{
+          margin-top:10px;
+          font-size:25px;
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <li class="nav-item">
+
+        <a  href="../admin/index.php">Inicio</a></li>
+        
+        <li class="nav-item">
+        <a href="../recepcionista/index.php">Recepcionistas</a></li>
     
-    $empieza=($pagina-1)*$registros;
-        $sql_total="SELECT * FROM servicios";
+        <li class="nav-item">
+        <a href="../admin/user.php">Usuarios</a></li>
 
-        $resultado=$base_de_datos->prepare($sql_total);
+        <li class="nav-item">
+        <a href="../client/index.php">Clientes</a></li>
 
-        $resultado->execute(array());
-        $numfilas=$resultado->rowCount();
-        $totalpagina=ceil($numfilas/$registros);
-
-        $registros=$base_de_datos->query("SELECT * FROM servicios LIMIT $empieza,$registros")->fetchALL(PDO::FETCH_OBJ);
+        <li class="nav-item">
+        <a href="../auxiliar/index.php">Auxiliares</a></li>
     
+        <li class="nav-item">
+        <a href="../mascotas/tablaM.php">Mascotas</a></li>
 
-    ?>
-    
-    <h3 align="center">Panel  de servicios</h3>
-    <form action=" " method="post" autocomplete="off">
-        <table border="2" align="center">
- 
-                <tr>
-                    <th>N. de servicio</th>
-                    <th>Nombre de servicios</th>
-                    <th>precio de servicio</th>
-                    
-                
-                </tr>
-           
-                <?php
-                    foreach ($registros as $servicios) :
-                    ?>
-                    <tr>
-                        <td><?php echo $servicios->id_servicio?></td>
-                    
-                    
-                        <td><?php echo $servicios->nombre_servicio?></td>
-                    
-                    
-                        <td><?php echo $servicios->precio_servicio?></td>
-                    
-                        <td>
-				            <a href="delete.php?id=<?php echo $servicios->id_servicio?>" class="elimina">
-					            <input type="button"  name="elimina" value="Eliminar">
-				            </a>
-                        </td>
-                        <td>
-				            <a href="editar.php?id=<?php echo $servicios->id_servicio?>" class="editar">
-					            <input type="button"  name="editar" value="Editar">
-				            </a>
-                        </td>
-                    </tr>
+        <li class="nav-item">
+        <a href="../razas/index.php">Razas</a></li>
 
-                    <?php
-			            endforeach;
-		
-			        ?>
-  
-        </table>
+        <li class="nav-item">
+        <a href="../servicios/tablaS.php">Servicios</a></li>
 
-    </form>
-    <div class="formulario" align="center">
-        <form action="registrar.php" method="get" >
-            <br>
-            
-            <a>Nombre de servicios:</a>
-            <input type="varchar" name="nomS" required><br>
-            
-           
-            <a>Precio de servicio:</a>
-            <input type="numbers" name="Pre" required><br>
-            
-            
-            <br><input  type="submit" class="button" name="Registrar" value="Registrar">
-            
-
-        </form>
+        <li class="nav-item">
+        <a href="../orden/index.php">Orden de servicios</a></li>
+        
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li><a href="../cerrar.php">Cerrar sesion</a></li>      
+      </ul>
     </div>
-    <script src="confirmacion.js"></script>
+  </div>
+</nav>
+<form method="post" action="buscar.php">
+        <div id="nim">
+            <a href="registrar.php">
+                <button class="btn btn-primary" type="button">
+                    Registrar
+                </button>
+            </a>
+        </div>
+        <input class="buscar"  type="search" name="busca"  id="" placeholder="Buscar"> 
+        <button id="but" class="btn btn-primary">buscar</button>
+        <br> <br>
+        <div id="tag">
+        <table  class="table">
+        <tr>
+            <th>N. de servicio</th>
+            <th>Servicio</th>
+            <th>Precio</th>
+            <th colspan="3">Accion</th>
+        </tr>
+        <?php
+            foreach ($resultado as $move) {
+        ?>
+        <tr>
+            <td><?php echo $move->id_servicio;?></td>
+            <td><?php echo $move->nombre_servicio?></td>
+            <td><?php echo $move->precio_servicio?></td>
+            <td>
+                    <a class="btn btn-primary" href="eliminar.php?id=<?php echo $move->id_servicio?> & nomb=<?php echo $move->nombre_servicio?>">
+					    eliminar
+                    </a>
+                </td>
+                <td>
+                    <a class="btn btn-primary" href="modificar.php?id=<?php echo $move->id_servicio?> & nomb=<?php echo $move->nombre_servicio?>">
+					    editar
+                    </a>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </div>
+    </table class="dos">
+		<td>
+			<a href="../admin/index.php" onmouseup="window.close()">
+                <input  class="btn btn-primary" id="bot" type="button" value="cerrar" name="cerrar" >
+            </a>
+		</td>
+	</table>
+    </form>
 </body>
-
 </html>
+       
         
